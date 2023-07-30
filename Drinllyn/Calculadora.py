abf81ee81c4dab8ec71a747d08eba7333bca17d4
Thomas Jefferson <68983072+drinllyn@users.noreply.github.com>
2023-07-30 05:17:38 -0300

    inicio do codigo

--- Drinllyn/Calculadora.py
+++ Drinllyn/Calculadora.py
@@ -1,1 +1,12 @@
 # Teste de calculadora em Python
+print('você gostaria de testar a calculadora?')
+inicio = input('Digite sim para continuar, ou não para sair')
+while inicio != "sim" or "não":
+    if inicio == "sim"
+        ...
+    elif inicio == "não"
+        print('Volte quando quiser testar!')
+        break
+    else
+        print('É necessario inserir sim ou não para continuar!')
+        continue
